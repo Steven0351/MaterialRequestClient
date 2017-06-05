fb5f62592235aa3a751886890bfc7e5e3e4734de
Steven Sherry <steven.sherry@gdt.com>
2017-06-05 15:47:24 -0500

    added if statements within try-block to reduce redundant code. Successful use of HttpComponents and Gson to make HTTP requests and parse JSON objects to be instantiated into TestObjects. Will test POST, PUT, and DELETE HTTP requests on MaterialRequestAPI.

--- src/com/stevensherry/httptests/TestHTTPMethods.java
+++ src/com/stevensherry/httptests/TestHTTPMethods.java
@@ -1,8 +1,6 @@
 package com.stevensherry.httptests;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -18,15 +16,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by steven.sherry on 6/5/2017.
  */
 public class TestHTTPMethods {
-
+static String oneCard = "https://api.scryfall.com/cards/named?exact=hindering+light&format=json";
+static String moreCards = "https://api.scryfall.com/cards/search?order=cmc&q=t%3Aartifact+pow%3E8+o%3Aflying";
     public static void main(String[] args) {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://api.scryfall.com/cards/named?exact=hindering+light&format=json");
+        HttpGet httpGet = new HttpGet(moreCards);
         TestObject testObject;
         ResponseHandler<JsonObject> rh = new ResponseHandler<JsonObject>() {
             @Override
@@ -49,19 +50,46 @@ public class TestHTTPMethods {
             }
         };
         testObject = new TestObject();
-        try {
-            JsonObject myJson = httpClient.execute(httpGet, rh);
-            testObject = new TestObject(myJson.get("id").getAsString(), myJson.get("uri").getAsString(),
-                                            myJson.get("multiverse_id").getAsInt(),myJson.get("name").getAsString(),
-                                            myJson.get("type_line").getAsString());
+        List<TestObject> list = new ArrayList<TestObject>();
 
-        } catch(IOException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(testObject);
-        }
+            try {
+                JsonObject myJson = httpClient.execute(httpGet, rh);
+                if(myJson.get("object").getAsString().equals("card")) {
+                    //         Instantiate single test object
+                    testObject = new TestObject(myJson.get("id").getAsString(),
+                                                myJson.get("uri").getAsString(),
+                                                myJson.get("multiverse_id").getAsInt(),
+                                                myJson.get("name").getAsString(),
+                                                myJson.get("type_line").getAsString());
+                } else if(myJson.get("object").getAsString().equals("list")) {
+                    // Instantiate JsonArray
+                    JsonArray jsonArray = myJson.getAsJsonArray("data");
 
+                    // Parse JsonArray elements and instantiate into TestObject's to add to list
+                    for (JsonElement card : jsonArray) {
+                        JsonObject cardObject = card.getAsJsonObject();
+                        TestObject loopObject = new TestObject(cardObject.get("id").getAsString(),
+                                                               cardObject.get("uri").getAsString(),
+                                                               cardObject.get("multiverse_id").getAsInt(),
+                                                               cardObject.get("name").getAsString(),
+                                                               cardObject.get("type_line").getAsString());
+                        list.add(loopObject);
+                    }
+                }
 
+            } catch(IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(list.isEmpty()) {
+                    System.out.println(testObject);
+                } else {
+                    for(TestObject card : list) {
+                        System.out.println(card);
+                    }
+                }
+            }
     }
 
 }
+
+
